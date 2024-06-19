aec675cfbe909746a418116af0078e239fac1e40
Douglas Pereira <douglasrodrigues20161@outlook.com>
2024-06-19 18:57:41 -0300

    adicionando tratamento por domain notifications

--- src/NerdStore.Core/Communication/Mediator/MediatorHandler.cs
+++ src/NerdStore.Core/Communication/Mediator/MediatorHandler.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using NerdStore.Core.Messages;
+using NerdStore.Core.Messages.CommonMessages.Notifications;
 
 namespace NerdStore.Core.Communication.Mediator
 {
@@ -20,6 +21,11 @@ namespace NerdStore.Core.Communication.Mediator
         public async Task<bool> EnviarComando<T>(T comando) where T : Command
         {
             return await _mediator.Send(comando);
+        }
+
+        public async Task PublicarNotificacao<T>(T notificacao) where T : DomainNotification
+        {
+            await _mediator.Publish(notificacao);
         }
     }
 }
